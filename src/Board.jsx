8b7b663fbb2a4bbdc28879723af69678de2d824c
Connor Baker <connorbaker@gmail.com>
2019-05-05 22:06:03 -0400

    implement single square selection

--- src/Board.jsx
+++ src/Board.jsx
@@ -3,14 +3,27 @@ import React from 'react';
 export class Board extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selected: false
+    }
+    this.flipSelected = this.flipSelected.bind(this);
   }
 
+  flipSelected() {
+    this.setState({
+      selected: !this.state.selected
+    });
+  }
+  
   render () {
     const arr = Array.from(new Array(8), () => [0,0,0,0,0,0,0,0]);
     const sqs = arr.map((row, i) => {
-      return<ul className="rank">{row.map((sq, j) => {
+      return <ul className="rank" key={i}>{row.map((sq, j) => {
         const klass = (i + j) % 2 === 0 ? "lightSquare" : "darkSquare";
-        return <Square klass={klass} key={(i + 1) * (j + 1)}/>
+        return <Square klass={klass} 
+                       selekt={this.state.selected}
+                       flipSelekt={this.flipSelected}
+                       key={(i + 1) * (j + 1)}/>
       })}</ul>
     });
     return (<div className="board">
@@ -29,9 +42,20 @@ class Square extends React.Component {
   }
 
   handleClick() {
-    this.setState({
-      selected: !this.state.selected
-    });
+    this.props.flipSelekt();
+    if (!this.props.selekt) {
+      this.setState({
+        selected: !this.state.selected
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!this.props.selekt && prevProps.selekt && this.state.selected) {
+      this.setState({
+        selected: false
+      });
+    }
   }
 
   render() {
@@ -41,5 +65,4 @@ class Square extends React.Component {
           onClick={this.handleClick}></li>
     )
   }
-
 }
